4fbb90037d8e77c5041c81203a95fcb02129a647
Yu Watanabe <yu.w.tennis@gmail.com>
2020-01-05 19:22:11 +0900

    Flink Runner testing passed.

--- flink-session-cluster/docker/samples/src/sample.py
+++ flink-session-cluster/docker/samples/src/sample.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
-
 import os
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.io.external.kafka import WriteToKafka
+from apache_beam.io.textio import WriteToText
 
 def main():
 
     sdk_endpoint = os.getenv('SDK_ENDPOINT')
     job_server   = os.getenv('JOB_SERVER')
+    project_id   = os.getenv('PROJECT_ID')
 
     options = PipelineOptions([
                       "--runner=PortableRunner",
@@ -22,7 +23,27 @@ def main():
 
     with beam.Pipeline(options=options) as p:
 
-        lines = ( p | beam.Create(['Hello World.']) )
+        lines = ( p | beam.Create(['Hello World.', 'Apache beam']) )
+
+        # Write to GCS
+        ( lines | WriteToText('gs://{}/sample.txt'.format(project_id))
+        )
+
+        #
+        # Send to kafka 
+        # WriteToKafka
+        # https://github.com/apache/beam/blob/master/sdks/python/apache_beam/io/external/kafka.py
+        # 
+        # Producer config
+        # https://kafka.apache.org/documentation/#producerconfigs
+        #
+        #( lines  | beam.Map(lambda x: ('payload', x) )
+        #         | WriteToKafka(
+        #              { 'acks': 'all', 'bootstrap.servers': 'localhost:9092'},
+        #              'beam',
+        #              'org.apache.kafka.common.serialization.ByteArraySerializer',
+        #              'org.apache.kafka.common.serialization.ByteArraySerializer',
+        #              'localhost:8097' ))
 
 if __name__ == "__main__":
 
